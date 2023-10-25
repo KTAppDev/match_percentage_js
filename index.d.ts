61a9c9a91dc462fd33da678eb3672ef581aafe19
Ken Andrew Taylor <kentaylorappdev@gmail.com>
2023-10-25 01:22:45 -0400

    switched the output match to boolean

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,4 @@
 
-declare function matchPercentage(sentence1: string, sentence2: string): { matchPercentage: number, match: string };
+declare function matchPercentage(sentence1: string, sentence2: string): { matchPercentage: number, match: boolean };
 
 export default matchPercentage;
